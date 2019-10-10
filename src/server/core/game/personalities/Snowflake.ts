007e085b22d124ed8a08a108c08ae4a3bcb21b08
jtelecsan <56180827+jtelecsan@users.noreply.github.com>
2019-10-09 21:13:50 -0400

    Create Snowflake.ts

---
+++ src/server/core/game/personalities/Snowflake.ts
@@ -0,0 +1,6 @@
+import { Personality } from '../../../../shared/interfaces';
+
+export class Snowflake extends Personality {
+  static description = 'You have a unique worth beyond words.  You always take the road less traveled.';
+  static toggleOff = ['Affirmer', 'Denier', 'Indecisive'];
+}
